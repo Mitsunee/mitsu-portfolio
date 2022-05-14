12381dc09394ac93d329288321c70fa5eca71607
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-05-14 20:31:41 +0200

    fix: about page global styles affecting other pages

--- pages/about/index.page.jsx
+++ pages/about/index.page.jsx
@@ -1,10 +1,10 @@
-import "./index.css";
+import styles from "./index.module.css";
 
 export function Page() {
   return (
-    <>
+    <div id={styles.about}>
       <h1>About</h1>
       <p>A colored page.</p>
-    </>
+    </div>
   );
 }
