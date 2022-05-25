b1d0b5c5dfe3ef17b021c5d851c603bbffbd242a
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-05-25 13:27:28 +0200

    wider layout and Columns component

--- renderer/Section.jsx
+++ renderer/Section.jsx
@@ -2,8 +2,12 @@ import cc from "classcat";
 
 export function Section({ children, className, ...props }) {
   return (
-    <section className={cc([className, "content-wrapper"])} {...props}>
+    <section className={cc([className, "content-section"])} {...props}>
       {children}
     </section>
   );
+}
+
+export function Columns({ children }) {
+  return <div className={"content-columns"}>{children}</div>;
 }
